bc1319a5763c2cb55e52984b06fb623ab400a1b4
antgit <iljinaa@gmail.com>
2016-02-21 10:38:50 +0200

    gulp

---
+++ src/TheBeeGame/gulpfile.js
@@ -0,0 +1,9 @@
+﻿/// <binding AfterBuild='less' />
+var gulp = require("gulp"),
+    less = require("gulp-less");
+
+gulp.task("less", function () {
+    gulp.src("./wwwroot/styles/styles.less")
+        .pipe(less())
+        .pipe(gulp.dest("./wwwroot/styles"));
+});
